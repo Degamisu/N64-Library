f121ee2590af0736031782a710fda870316b9f0c
Degamisu <degamisu@gmail.com>
2023-10-26 18:08:18 -0400

    Add all

---
+++ examples/example_01.c
@@ -0,0 +1,30 @@
+// n64library.c (Source code for your library)
+#include "n64library.h"
+
+// Define your library's functions here
+
+// Function 1: Example function
+// You should replace this with your actual library functions
+int exampleFunction(int parameter1, char parameter2)
+{
+    // Your implementation here
+    return 0; // Replace with actual return value
+}
+
+// Function 2: Another example function
+// You should replace this with your actual library functions
+void anotherFunction()
+{
+    // Your implementation here
+}
+
+// You can add more functions as needed
+
+// Main function (used for testing)
+int main()
+{
+    // You can test your library functions here
+    int result = exampleFunction(42, 'A');
+    // Print the result or perform other testing here
+    return 0;
+}
